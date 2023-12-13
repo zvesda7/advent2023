d02e4f89f08bfa7a6054b6aece0002ebe98d91c7
Vernon Gilbert <zvesda7@gmail.com>
2023-12-12 22:30:51 -0600

    day12 in prog

--- day12/day12.go
+++ day12/day12.go
@@ -13,93 +13,90 @@ func Run() {
 	//fmt.Println(combos(7, []int{2, 1}))
 
 	sum := 0
-	//for _, row := range input {
-	//	pat, counts := parseRow(row)
-	//	combos := calcCombos(pat, counts)
-	//	sum += combos
-	//}
-	//fmt.Println("Part 1", sum)
+	for _, row := range input {
+		pat, counts := parseRow(row)
+		combos := calcCombos(pat, counts)
+		sum += combos
+	}
+	fmt.Println("Part 1", sum)
 
 	sum = 0
-	for _, row := range input[3:4] {
+	for _, row := range input {
 		pat, counts := parseRow(row)
-		newPat := pat
-		newCounts := counts
-		for i := 0; i < 4; i++ {
-			newPat += "?" + pat
-			newCounts = append(newCounts, counts...)
+
+		combos1 := calcCombos(pat, counts)
+		combos2 := calcCombos(pat+"?"+pat, append(counts, counts...))
+		combos3 := calcCombos(pat+"?"+pat+"?"+pat, append(append(counts, counts...), counts...))
+		combos4 := calcCombos(pat+"?"+pat+"?"+pat+"?"+pat, append(append(append(counts, counts...), counts...), counts...))
+		ratio := combos2 / combos1
+		ratio2 := combos3 / combos2
+		ratio3 := combos4 / combos3
+		if ratio != ratio2 {
+			fmt.Println(pat, counts, combos1, combos2, combos3, combos4, ratio, ratio2, ratio3, "bad")
+		} else {
+			fmt.Println(pat, counts, combos1, ratio, ratio2, "good")
 		}
-		combos := calcCombos(newPat, newCounts)
-		fmt.Println(newPat, newCounts, combos)
-		sum += combos
+		total := combos1
+		for i := 0; i < 4; i++ {
+			total *= ratio
+		}
+
+		sum += total
 	}
 	fmt.Println("Part 2", sum)
 }
 
-func match(pat string, actual string) bool {
-	for i := 0; i < len(pat); i++ {
-		if pat[i] != actual[i] && pat[i] != '?' {
-			return false
-		}
-	}
-	return true
-}
-
 func calcCombos(pat string, counts []int) int {
-	comboS := combos(len(pat), counts)
-	count := 0
-	for _, combo := range comboS {
-		if match(pat, combo) {
-			count++
-		}
-	}
-	return count
-}
-
-func combos(n int, counts []int) []string {
+	n := len(pat)
 	for _, c := range counts {
 		n -= c
 	}
 	n -= len(counts) - 1 //spaces that have to exist.
 	k := len(counts) + 1 //gap before first
-	perms := [][]int{}
-	fmt.Println("complexity", n, k)
-	perm(n, k, []int{}, &perms)
 
-	final := []string{}
-	for _, p := range perms {
-		s := ""
-		for i, c := range p {
-			s += strings.Repeat(".", c)
-			if i > 0 && i < len(p)-1 {
-				s += "."
-			}
-			if i < len(counts) {
-				s += strings.Repeat("#", counts[i])
-			}
-		}
-		final = append(final, s)
-		//fmt.Println(s, p)
-	}
-
-	return final
+	return perm([]byte(pat), counts, n, k, []int{})
 }
 
-func perm(n int, k int, prefix []int, running *[][]int) {
+func perm(pat []byte, counts []int, n int, k int, prefix []int) int {
+
 	if k == 1 {
-		prefix := append(prefix, n)
-		temp := make([]int, len(prefix))
-		copy(temp, prefix)
-		(*running) = append((*running), temp)
-		//fmt.Println(n, k, temp)
-	} else {
-		for x := 0; x <= n; x++ {
-			if n-x >= 0 {
-				newPre := append(prefix, x)
-				perm(n-x, k-1, newPre, running)
+		newPre := append(prefix, n)
+
+		if checkMatch(pat, counts, newPre) {
+			return 1
+		} else {
+			return 0
+		}
+	}
+	total := 0
+	for x := 0; x <= n; x++ {
+		if n-x >= 0 {
+			newPre := append(prefix, x)
+			if checkMatch(pat, counts, newPre) {
+				total += perm(pat, counts, n-x, k-1, newPre)
 			}
 		}
 	}
+	return total
+}
+
+func checkMatch(pat []byte, counts []int, prefix []int) bool {
+	s := ""
+	for i, c := range prefix {
+		s += strings.Repeat(".", c)
+		if i > 0 && i < len(counts) {
+			s += "."
+		}
+		if i < len(counts) {
+			s += strings.Repeat("#", counts[i])
+		}
+	}
+	for i := 0; i < len(s); i++ {
+		if pat[i] != s[i] && pat[i] != '?' {
+			return false
+		}
+	}
+	return true
 }
 
 func parseRow(row string) (string, []int) {
