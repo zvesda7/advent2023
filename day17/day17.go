e426a99010d303ebde1fec78219899246b7f1f3e
Vernon Gilbert <zvesda7@gmail.com>
2023-12-17 15:40:02 -0600

    day17 checkpoint2

--- day17/day17.go
+++ day17/day17.go
@@ -24,7 +24,6 @@ func Run() {
 	width := len(input[0])
 	height := len(input)
 	efforts := map[PHash]int{}
-	dists := map[PHash]int{}
 	for y := 0; y < len(input); y++ {
 		for x := 0; x < len(input[y]); x++ {
 			p := Point{x, y}
@@ -34,16 +33,27 @@ func Run() {
 
 	points := []Point{}
 	p0 := Point{0, 0}
-	dists[p0.hash()] = 0
 	points = append(points, p0)
+
+	dists := map[PHash]int{}
+	dists[p0.hash()] = 0
+
+	prevP := map[PHash]Point{}
+	prevP[p0.hash()] = Point{-1, -1}
+
 	for len(points) > 0 {
 		newPoints := []Point{}
 		for i := 0; i < len(points); i++ {
 			adj := neighbors(points[i], width, height)
+			p1 := prevP[points[i].hash()]
+			p2 := prevP[p1.hash()]
 			for j := 0; j < len(adj); j++ {
 				if _, found := dists[adj[j].hash()]; !found {
-					dists[adj[j].hash()] = dists[points[i].hash()] + efforts[adj[j].hash()]
-					newPoints = append(newPoints, adj[j])
+					if !inRow([]Point{adj[j], points[i], p1, p2}) {
+						dists[adj[j].hash()] = dists[points[i].hash()] + efforts[adj[j].hash()]
+						newPoints = append(newPoints, adj[j])
+						prevP[adj[j].hash()] = points[i]
+					}
 				}
 			}
 		}
@@ -59,6 +69,22 @@ func Run() {
 		}
 		fmt.Printf("\n")
 	}
+}
+
+func inRow(points []Point) bool {
+	xSame := true
+	ySame := true
+	x := points[0].x
+	y := points[0].y
+	for _, p := range points {
+		if p.x != x {
+			xSame = false
+		}
+		if p.y != y {
+			ySame = false
+		}
+	}
+	return xSame || ySame
 }
 
 func neighbors(p Point, width int, height int) []Point {
