c9df954d34d4a8a9e46286073e68cb8a374cd86d
Vernon Gilbert <zvesda7@gmail.com>
2023-12-17 21:29:53 -0600

    day17 checkpoint 3

--- day17/day17.go
+++ day17/day17.go
@@ -2,7 +2,9 @@ package day17
 
 import (
 	"advent23/utils"
+	"bufio"
 	"fmt"
+	"os"
 	"sort"
 )
 
@@ -42,30 +44,83 @@ func Run() {
 	prevP[p0.hash()] = Point{-1, -1}
 
 	for len(points) > 0 {
-		newPoints := []Point{}
-		for i := 0; i < len(points); i++ {
-			adj := neighbors(points[i], width, height)
-			p1 := prevP[points[i].hash()]
-			p2 := prevP[p1.hash()]
-			for j := 0; j < len(adj); j++ {
-				if _, found := dists[adj[j].hash()]; !found {
-					if !inRow([]Point{adj[j], points[i], p1, p2}) {
-						dists[adj[j].hash()] = dists[points[i].hash()] + efforts[adj[j].hash()]
-						newPoints = append(newPoints, adj[j])
-						prevP[adj[j].hash()] = points[i]
-					}
+
+		adj := neighbors(points[0], width, height)
+		//p1 := prevP[points[0].hash()]
+		//p2 := prevP[p1.hash()]
+		for j := 0; j < len(adj); j++ {
+			if _, found := dists[adj[j].hash()]; !found {
+				if allowed(points[0], adj[j], dists, efforts, width, height, 0) {
+					dists[adj[j].hash()] = dists[points[0].hash()] + efforts[adj[j].hash()]
+					points = append(points, adj[j])
+					//prevP[adj[j].hash()] = points[0]
 				}
 			}
 		}
-		points = newPoints
+		points = points[1:]
 		sort.Slice(points, func(i, j int) bool {
 			return dists[points[i].hash()] < dists[points[j].hash()]
 		})
+		print(width, height, dists, efforts)
+		bufio.NewReader(os.Stdin).ReadBytes('\n')
+	}
+
+	path := map[PHash]bool{}
+	pp := Point{width - 1, height - 1}
+	for !(pp.x == 0 && pp.y == 0) {
+		path[pp.hash()] = true
+		pp = prevP[pp.hash()]
 	}
 	for y := 0; y < height; y++ {
 		for x := 0; x < width; x++ {
+			//p := Point{x, y}
+			//if _, ok := path[p.hash()]; ok {
+			//	fmt.Printf("\033[32m")
+			//}
+			//fmt.Printf("[%3d]", dists[p.hash()])
+			//fmt.Printf("\033[0m")
+		}
+		//fmt.Printf("\n")
+	}
+}
+
+func allowed(s Point, d Point, dists map[PHash]int, efforts map[PHash]int, width int, height int, depth int) bool {
+	adj := neighbors(s, width, height)
+	tadj := []Point{}
+	sDist := dists[s.hash()]
+	sEff := efforts[s.hash()]
+	for i := 0; i < len(adj); i++ {
+		tDist := dists[adj[i].hash()]
+		if tDist+sEff == sDist {
+			tadj = append(tadj, adj[i])
+		}
+	}
+	if len(tadj) == 0 {
+		return true
+	}
+	for i := 0; i < len(tadj); i++ {
+		if !inRow([]Point{tadj[i], s, d}) {
+			return true
+		}
+	}
+	if depth == 0 {
+		return allowed(tadj[0], s, dists, efforts, width, height, depth+1)
+	} else {
+		return false
+	}
+}
+
+func print(width int, height int, dists map[PHash]int, efforts map[PHash]int) {
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
 			p := Point{x, y}
-			fmt.Printf("[%3d]", dists[p.hash()])
+			if _, ok := dists[p.hash()]; ok {
+				fmt.Printf("\033[32m")
+				fmt.Printf("[%3d]", dists[p.hash()])
+			} else {
+				fmt.Printf("\033[0m")
+				fmt.Printf("[%3d]", efforts[p.hash()])
+			}
 		}
 		fmt.Printf("\n")
 	}
